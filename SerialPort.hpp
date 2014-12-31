4a231cd8feb297160e2605d2b6926b646fa5de78
Glyfina <glyfina.f@gmail.com>
2014-12-31 20:23:36 +0530

    Create SerialPort.hpp

---
+++ SerialPort.hpp
@@ -0,0 +1,24 @@
+
+
+#pragma once
+#include "windows.h"
+
+class CSerialPort
+{
+public:
+	CSerialPort(void);
+	~CSerialPort(void);
+
+	// prototypes
+	void OpenSerial( void );
+	void Send(unsigned char* data );
+	void Send(char* data );
+	void CloseSerial( void );
+
+protected:
+
+	HANDLE SerialPort;
+	int Error;
+	unsigned long dwNumberOfBytesWritten;
+};
+
